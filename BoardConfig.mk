e4d0bda88de7486c7daba8fb3b5e1ee1fac54e71
Daniel Schleif <seadersn@gmail.com>
2018-11-01 17:57:40 +0100

    bring to actual state

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,17 +30,18 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Kernel
-TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/prebuilt/Image.gz-dtb
+#TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/prebuilt/Image.gz-dtb
 #TARGET_KERNEL_APPEND_DTB := true
-BOARD_KERNEL_IMAGE_NAME := Image.gz
-#TARGET_KERNEL_SOURCE := kernel/lge/msm8998
+#BOARD_SEPOLICY_DIRS += device/lge/joan/sepolicy
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_SOURCE := kernel/lge/msm8998
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-#TARGET_KERNEL_CONFIG := twrp_h930_defconfig
-#KERNEL_TOOLCHAIN_PREFIX := /home/seadersn/bin/gcc-linaro-6.4.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CONFIG := lineageos_h930_defconfig
+KERNEL_TOOLCHAIN_PREFIX := /home/seadersn/bin/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 # Boot image
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.hardware=joan androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.hardware=joan androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100
@@ -53,15 +54,18 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x1BC4E74000
 BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x0020000000
 BOARD_FLASH_BLOCK_SIZE := 0x40000
 
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP specific build flags
-#PLATFORM_SDK_VERSION := 28
 TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true 
+RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
@@ -70,7 +74,8 @@ TW_MAX_BRIGHTNESS := 249
 TW_DEFAULT_BRIGHTNESS := 130
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INCLUDE_NTFS_3G := true
-#TARGET_RECOVERY_DEVICE_MODULES := strace
+TARGET_RECOVERY_DEVICE_MODULES := libcryptfs_hw strace
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_OPTIONAL_EXECUTABLES)/strace
 
 # We can use the factory reset button combo to enter recovery safely
 TW_IGNORE_MISC_WIPE_DATA := true
@@ -79,11 +84,12 @@ TW_IGNORE_MISC_WIPE_DATA := true
 TW_NO_EXFAT_FUSE := true
 
 # Encryption support
+TARGET_USE_UFS_ICE := true
 TW_INCLUDE_CRYPTO := true
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd hwservicemanager keymaster-3-0
-WITH_CRYPTO_UTILS := true
+#WITH_CRYPTO_UTILS := true
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
@@ -91,5 +97,7 @@ TW_EXTRA_LANGUAGES := true
 # Debug flags
 #TWRP_INCLUDE_LOGCAT := true
 #TARGET_USES_LOGD := true
-#TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
 TW_DEVICE_VERSION := 5-default
+#TWRP_EVENT_LOGGING := true
+#TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
